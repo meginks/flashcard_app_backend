d167a1132e4f3dd6c0eb6a910bb8fc36d9de62fb
meginks <meganjones.dev@gmail.com>
2020-02-09 05:21:55 -0800

    added 4 words to seeds for nouns

--- seeds/words.js
+++ seeds/words.js
@@ -1,13 +1,39 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('nouns').del()
     .then(function () {
       // Inserts seed entries
       return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      {
+        id: 1, 
+        Italian: 'il ragazzo',
+        plural_Italian: 'i ragazzi', 
+        English: 'boy',
+        plural_English: 'boys',
+      }, 
+      {
+        id: 2, 
+        Italian: 'la ragazza',
+        plural_Italian: 'le ragazze', 
+        English: 'girl',
+        plural_English: 'girls',
+      }, 
+      {
+        id: 3, 
+        Italian: "l'anno",
+        plural_Italian: "gli anni", 
+        English: "year",
+        plural_English: "years",
+      }, 
+      {
+        id: 4, 
+        Italian: 'il caffè',
+        plural_Italian: 'caffè', 
+        English: 'coffee',
+        plural_English: 'coffee',
+      } 
+
       ]);
     });
 };
